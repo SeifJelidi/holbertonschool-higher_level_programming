d35c645108ea1c1fdf710ad6082b826e90a4e146
Seif Jelidi <seifjelidi3@gmail.com>
2020-04-27 18:42:26 +0000

    fix

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(str[0:9] + '\t' + str[10:16] + str[0:9] + '\t' + str[10:16] + str[0:9] + '\t' + str[10:16] + '\n' + str[0:9])
+print(str + str + str + '\n' + str[0:9] + '\n')
