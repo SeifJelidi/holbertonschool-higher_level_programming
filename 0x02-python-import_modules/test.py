aa641e2a190d41bb9ebad324d6e6b9d21490aeea
Seif Jelidi <seifjelidi3@gmail.com>
2020-04-30 22:11:04 +0000

    argv

---
+++ 0x02-python-import_modules/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print("{:d} arguments.".format(len(argv) - 1))
+    else:
+        if len(argv) == 2:
+            ch = " argument:"
+        else:
+            ch = " arguments:"
+        print("{}".format(len(argv) - 1) + ch)
+        for i, j in enumerate(argv):
+            if i != 0:
+                print("{}: {}".format(i, j)) 
+    
