e025d6156efe562888f2a96df7022182af4b6c47
Seif Jelidi <seifjelidi3@gmail.com>
2020-04-30 17:41:14 +0000

    calculator

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from calculator_1 import *
+#  the '*' is not a pointer like in c ( just to select all funcs into the file)
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
