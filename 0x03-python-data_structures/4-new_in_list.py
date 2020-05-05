228c917596e100db83908a09473ca140e11d5f7b
Seif Jelidi <seifjelidi3@gmail.com>
2020-05-05 18:38:39 +0000

    fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,5 @@ def new_in_list(my_list, idx, element):
     new_list = my_list[:]
     if idx < 0 | idx + 1 > len(my_list):
         return my_list
-    else:
-        new_list[idx] = element
-        return new_list
+    new_list[idx] = element
+    return new_list
