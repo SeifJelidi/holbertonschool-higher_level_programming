41ea7eb48ac9f69cf63c03aeb91b65e5a221649e
Seif Jelidi <seifjelidi3@gmail.com>
2020-05-05 21:19:07 +0000

    max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return(None)
+    x = my_list[1]
+    for y in my_list:
+        if x < y:
+            x = y
+    return x
