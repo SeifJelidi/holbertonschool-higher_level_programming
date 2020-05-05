3a7c3f32f266739edc9c7a0af35896c6157b399d
Seif Jelidi <seifjelidi3@gmail.com>
2020-05-05 17:36:23 +0000

    fix code

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
         my_list.sort(reverse=True)
         for rev in my_list: 
-            print("{}".format(rev))
+            print("{:d}".format(rev))
