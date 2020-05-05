b4fb16d5c63ee3ce7a5df1f0871eed0cad2211ae
Seif Jelidi <seifjelidi3@gmail.com>
2020-05-05 17:44:35 +0000

    fixes un pycodestyle

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-        my_list.sort(reverse=True)
-        for rev in my_list: 
-            print("{:d}".format(rev))
+    my_list.sort(reverse=True)
+    for rev in my_list:
+        print("{:d}".format(rev))
