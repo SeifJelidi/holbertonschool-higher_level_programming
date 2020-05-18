cf3be5c1fab40fb481ff92b88c310eddb5448e27
Seif Jelidi <seifjelidi3@gmail.com>
2020-05-18 19:28:23 +0000

    fix 3

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         nx = 0
         while nx < x:
             print(my_list[nx], end='')
-            nx += 1
+            nx = nx + 1
         print()
     except:
         print()
