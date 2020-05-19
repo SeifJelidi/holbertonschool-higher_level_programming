5f935048ead8db62ece15187141b13ae5a732030
Seif Jelidi <seifjelidi3@gmail.com>
2020-05-19 21:07:23 +0000

    2-square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+'''2-square: create a class Square'''
+
+
 class Square:
+    '''define class Square'''
     def __init__(self, size=0):
+        '''initialize square class'''
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         
