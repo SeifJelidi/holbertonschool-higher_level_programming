aef89c9374cce11d4edb8510768e0eccd79cee64
Seif Jelidi <seifjelidi3@gmail.com>
2020-05-19 20:50:32 +0000

    1-square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+
+'''1-square: create a class Square'''
+
 class Square:
+    '''define class Square'''
     def __init__(self, __size):
+        '''initialize square class'''
         self.__size = __size
