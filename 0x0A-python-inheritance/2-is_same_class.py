49d308450904dd3d3edfc9f2f96bf5af62ce7be4
Seif Jelidi <seifjelidi3@gmail.com>
2020-06-02 18:17:40 +0000

    task2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''returns true or famse'''
+
+
+def is_same_class(obj, a_class):
+    '''if the object is exactly an instance of the specified class return True ,otherwise return False '''
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
