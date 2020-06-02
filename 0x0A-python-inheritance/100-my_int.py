4bf085b3cc471fa9e06f0aef4dd9106e907760be
Seif Jelidi <seifjelidi3@gmail.com>
2020-06-02 21:58:44 +0000

    add comments

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,14 +4,19 @@ the parent class (int)'''
 
 
 class MyInt(int):
+    '''__'''
     def __init__(self, number):
+        '''__'''
         self.number = number
 
     def __ne__(self, val):
+        '''___'''
         return (self.number == val)
 
     def __eq__(self, val):
+        '''___''''
         return (self.number != val)
 
     def __str__(self):
+        '''___'''
         return (str(self.number))
