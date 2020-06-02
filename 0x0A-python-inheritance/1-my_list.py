c3ffa9a4ac95394ac512f4f21f950d6eb4d36acf
Seif Jelidi <seifjelidi3@gmail.com>
2020-06-02 18:02:56 +0000

    2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''MyList that inherits from list'''
+
+
+class MyList(list):
+    '''the child class from the parent class (list)'''
+    def print_sorted(self):
+        '''print the list'''
+        print (sorted(self))
