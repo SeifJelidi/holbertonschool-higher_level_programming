45c779623e0863da0dd9cbfe28f9b35a18501449
Seif Jelidi <seifjelidi3@gmail.com>
2020-06-02 21:14:00 +0000

    inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''__inheritance__'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''child Class from the rectangle class'''
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        '''return area'''
+        return self.__size * self.__size
