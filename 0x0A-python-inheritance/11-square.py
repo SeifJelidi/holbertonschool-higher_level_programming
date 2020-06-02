a798dcf0060bb301a7a61186da69f5cd31f2e6e9
Seif Jelidi <seifjelidi3@gmail.com>
2020-06-02 21:31:26 +0000

    fix

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -15,6 +15,7 @@ class Square(Rectangle):
     def area(self):
         '''return area'''
         return self.__size * self.__size
+
     def __str__(self):
         '''___'''
         return "[Square] {}/{}".format(self.__size, self.__size)
