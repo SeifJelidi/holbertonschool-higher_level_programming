fca2dff6607a5cd78a498b5e83f41399ef5aff1c
Seif Jelidi <seifjelidi3@gmail.com>
2020-06-03 22:33:29 +0000

    task 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""___"""
+
+
+class Student():
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialize Student attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            if hasattr(self, '__dict__'):
+                return self.__dict__
+            if hasattr(self, '__slots__'):
+                return self.__slots__
+
+        if attrs is not None and isinstance(attrs, list) is False:
+            if hasattr(self, '__dict__'):
+                return self.__dict__
+            if hasattr(self, '__slots__'):
+                return self.__slots__
+
+        if attrs is not None:
+            if all(isinstance(item, str) for item in attrs):
+                a = {}
+                for pos, v in self.__dict__.items():
+                    if pos in attrs:
+                        a[pos] = v
+                return a
