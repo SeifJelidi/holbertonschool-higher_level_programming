028b77f0bbca11f28dc00d71739054a61886ef2e
Seif Jelidi <seifjelidi3@gmail.com>
2020-06-03 21:53:28 +0000

    task 6

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""___"""
+
+
+def from_json_string(my_str):
+    """returns an object (Python data structure)
+       represented by a JSON string"""
+    import json
+    return json.loads(my_str)
