7f741d88a09ae987274cae7070bb98cb752ae96b
Seif Jelidi <seifjelidi3@gmail.com>
2020-06-03 20:24:18 +0000

    fix style

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+'''___'''
+
+
 def number_of_lines(filename=""):
     """number of lines"""
     ligne = 0
-    with open(filename,'r', encoding='utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         for l1 in f:
             ligne += 1
     return ligne
