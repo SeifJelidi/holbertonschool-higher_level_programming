519db49b092399b517baa83445bb32443175f40f
Seif Jelidi <seifjelidi3@gmail.com>
2020-06-03 18:50:41 +0000

    fix3

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     """reading file"""
-    with open(filename, mode="r", encoding='utf-8') as f:
-        reading = f.read()
-    print(reading, end='')
+    with open(filename,encoding='utf-8') as f:
+        print(f.read(), end='')
