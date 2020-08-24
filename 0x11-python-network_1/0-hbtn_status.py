821283bd56e9817192a35fca860371688fc70226
SeifJelidi <seifjelidi3@gmail.com>
2020-08-24 10:51:38 -0400

    fetch hbtn

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import urllib.request
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        s = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(s)))
+        print("\t- content: {}".format(s))
+        print("\t- utf8 content: {}".format(s.decode("utf-8")))
