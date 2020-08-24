0e189cdc4b63783b9f5ba88db1a736e270466720
SeifJelidi <seifjelidi3@gmail.com>
2020-08-24 16:18:17 -0400

    test

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+displays X-Request-id's value
+"""
+import urllib.request
+import urllib.parse
+from sys import argv
+if __name__ == "__main__":
+
+    data = parse.urlencode({'email': sys.argv[2]}).encode("ascii")
+    req = request.Request(sys.argv[1], data=data, method='POST')
+    with request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
