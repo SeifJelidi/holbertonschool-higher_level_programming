d548f9cc535883f2357ac65aaa0f1681a0144469
Seif Jelidi <seifjelidi3@gmail.com>
2020-09-15 19:23:44 +0000

    warm up

--- test.py
+++ test.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 class User:
-    def __init__(self, full_name, birthday):
-        self.name = full_name
-        self.birthday = birthday
-user = User("seif", "16061995")
-print(user.name)
-print(user.birthday)
-user1 = User("mohamed", "565974")
-print(user1.name)
-print(user1.birthday)
-help(User)
+    id = 1
+
+u = User()
+u.id = 89
+User.id = 98
+print(u.id)
